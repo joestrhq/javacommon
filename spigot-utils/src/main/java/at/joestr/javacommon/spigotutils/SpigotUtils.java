b954c576df0397984affa013aa9e2581386b7a4f
Joel Strasser <strasser999@gmail.com>
2022-10-09 16:31:55 +0200

    adds spigot-utils module

---
+++ spigot-utils/src/main/java/at/joestr/javacommon/spigotutils/SpigotUtils.java
@@ -0,0 +1,15 @@
+//
+// Copyright (c) 2022 Joel Strasser <strasser999@gmail.com>
+//
+// Licensed under the EUPL-1.2 license.
+//
+// For the full license text consult the 'LICENSE' file from the repository.
+//
+package at.joestr.javacommon.spigotutils;
+
+/**
+ *
+ * @author joestr
+ */
+public class SpigotUtils {
+}
